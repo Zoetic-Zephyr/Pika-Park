a276ae525c0599c98bbc679d2feee694e15facec
Jack Zhang <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-04-06 23:53:08 -0400

    Reverse geolocate, display street address

--- Pika Park/MapScreen.swift
+++ Pika Park/MapScreen.swift
@@ -17,6 +17,7 @@ class MapScreen: UIViewController {
     
     let locationManager = CLLocationManager()
     let regionMeters: Double = 10000
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,11 +58,7 @@ class MapScreen: UIViewController {
         switch CLLocationManager.authorizationStatus() { // device-level location permissions
         case .authorizedWhenInUse:
             // Do Map Stuff
-            // 6.
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-            break
+            startTrackingUserLocation()
         case .denied:
             // show alert instructing them how to turn on permission
             break
@@ -77,18 +74,33 @@ class MapScreen: UIViewController {
         }
     }
     
+    func startTrackingUserLocation() {
+        // 6.
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        // 11.2
+        previousLocation = getCenterLocation(for: mapView)
+    }
     
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 }
 
 // 1.
 extension MapScreen: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // We'll be back
-        guard let location = locations.last else { return } // guard
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeters, longitudinalMeters: regionMeters)
-        mapView.setRegion(region, animated: true)
-    }
+// 8.
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        // We'll be back
+//        guard let location = locations.last else { return } // guard
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeters, longitudinalMeters: regionMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         // We'll be back
@@ -96,3 +108,44 @@ extension MapScreen: CLLocationManagerDelegate {
     }
 }
 
+// 9.
+extension MapScreen: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        // 10.1
+        let geoCoder = CLGeocoder()
+        
+        // 13.
+        guard let previousLocation = self.previousLocation else { return }
+        
+        
+        // 11.1
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        // 10.2 guards are for unrelated error handling
+        geoCoder.reverseGeocodeLocation(center) {[weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let _ = error {
+                // TODO: show alert informing user
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                // TODO: show alert informing user
+                return
+            }
+            
+            // 11.3
+            let streetNumber = placemark.subThoroughfare ?? "" // nil coalescing
+            let streetName = placemark.thoroughfare ?? "" // nil coalescing
+            
+            // 12. Closure, Async, main thread blah blah...
+            DispatchQueue.main.async {
+                self.adressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
+    }
+}
