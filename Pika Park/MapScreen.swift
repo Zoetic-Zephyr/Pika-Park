fc44c4b31bab4c92dbb1f0534f78a4be119823d4
Jack Zhang <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-04-06 20:50:03 -0400

    Setup location manager, check location authorization

--- Pika Park/MapScreen.swift
+++ Pika Park/MapScreen.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapScreen: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 5.
+        checkLocationServices()
     }
+    
+    
+    // 3.
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    // 2.
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            // setup our location manager
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // show alert letting the user know they have to turn this on
+        }
+    }
+    
+    // 4.
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            // Do Map Stuff
+            break
+        case .denied:
+            // show alert instructing them how to turn on permission
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            // show alert letting user know maybe "parental control" is activated
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    
+}
 
-
+// 1.
+extension MapScreen: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // We'll be back
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // We'll be back
+    }
 }
 
