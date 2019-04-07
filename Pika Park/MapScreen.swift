88469e4fd0f9b36b77836cac0ff824d2b45955a4
Jack Zhang <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-04-06 20:11:08 -0400

    Setup workspace

---
+++ Pika Park/MapScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MapScreen.swift
+//  Pika Park
+//
+//  Created by 张正 on 2019/4/6.
+//  Copyright © 2019 JackZhang. All rights reserved.
+//
+
+import UIKit
+
+class MapScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
