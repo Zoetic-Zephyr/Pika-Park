8711a59428729e3c658e6b2c9509ee19c3f85332
Jack Zhang <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-05-06 13:01:05 -0400

    Install pod 'SwiftMessages'

--- Pods/Target Support Files/Pods-Pika Park/Pods-Pika Park-frameworks.sh
+++ Pods/Target Support Files/Pods-Pika Park/Pods-Pika Park-frameworks.sh
@@ -154,9 +154,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RevealingSplashView/RevealingSplashView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RevealingSplashView/RevealingSplashView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
