a61e414dda765c4cbc8b70f618cb80cd1a045704
Jack Zhang <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-05-06 11:23:42 -0400

    Add button animation, further hide & show

---
+++ Pika Park/UIButtonExtension.swift
@@ -0,0 +1,25 @@
+//
+//  UIButtonExtension.swift
+//  Pika Park
+//
+//  Created by 张正 on 2019/5/6.
+//  Copyright © 2019 JackZhang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.2
+        pulse.fromValue = 1.0
+        pulse.toValue = 0.9
+        pulse.autoreverses = true
+        pulse.repeatCount = 1
+        pulse.initialVelocity = 1.0
+        pulse.damping = 1.0
+        
+        layer.add(pulse, forKey: nil)
+    }
+}
